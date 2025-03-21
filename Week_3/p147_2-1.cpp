2445aa9bba4fcfaae19602f07a2a8f67de689456
SEONGMINSEONG <sms92213441@gmail.com>
2025-03-21 18:29:42 +0900

    주석 추가

--- Week_3/p147_2-1.cpp
+++ Week_3/p147_2-1.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-using namespace std;
+using namespace std;      //헤더 파일 포함
 
-int reverse(int n) {
-    int rev = 0;
-    while (n != 0) {
-        rev = rev * 10 + n % 10;
-        n /= 10;
+int reverse(int n) {      //reverse 함수는 숫자의 자릿수를 뒤집는 역할
+    int rev = 0;          // 1. rev를 0으로 초기화
+    while (n != 0) {      // n이 0이 될 때까지 계속 반복한다는 의미
+        rev = rev * 10 + n % 10;  // 2. 마지막 자릿수(n % 10)를 rev에 더함
+        n /= 10;                  // 마지막 자릿수를 제거
     }
-    return rev;
+    return rev;           // 최종적으로 뒤집힌 숫자 반환
 }
 
-int main() {
+int main() {              // 프로그램의 시작점
     int num;
-    cout << "Enter a number: ";
+    cout << "Enter a number: ";   // 
     cin >> num;
     cout << "Reversed: " << reverse(num) << endl;
     return 0;
 }
-
--- Week_3/p147_5-1.h
+++ Week_3/p147_5-1.h
@@ -1,7 +1,7 @@
 #ifndef P147_5_1_H
 #define P147_5_1_H
 
-// 러시안 룰렛 게임을 위한 함수 선언
+
 void playRussianRoulette(int n);
 
-#endif // P147_5_1_H
+#endif 
